67e114eff8fd4f350db0e06db03ac1cf199764b6
saad elnady <saad.elnady.work@gmail.com>
2025-01-05 01:19:33 +0200

    implement pagination in products page

--- pages/products/index.js
+++ pages/products/index.js
@@ -11,7 +11,7 @@ export default products;
 
 export const getStaticProps = wrapper.getStaticProps((store) => {
   return async () => {
-    store.dispatch(getAllProducts({ cookies: {} }));
+    store.dispatch(getAllProducts({ cookies: {}, page: 1, limit: 12 }));
     store.dispatch(END);
     await store.sagaTask.toPromise();
     return {
--- src/components/productsPage/index.js
+++ src/components/productsPage/index.js
@@ -4,22 +4,23 @@ import ProductCard from "./productCard";
 import styles from "./styles/styles.module.scss";
 import { Col, Container, Row } from "react-bootstrap";
 import { getAllProducts } from "@/store/actions";
+import { useRouter } from "next/router";
 const ProductsPage = () => {
   const dispatch = useDispatch();
   const { products, metadata, results } = useSelector(
     (state) => state.products
   );
-
+  const { locale } = useRouter();
   const handlePageChange = (page) => {
     if (page >= 1 && page <= metadata.numberOfPages) {
-      dispatch(getAllProducts({ cookies: {}, page, limit: results }));
+      dispatch(getAllProducts({ cookies: {}, page, limit: 12 }));
     }
   };
   return (
     <div className={styles.products}>
       <Container>
         <Row>
-          {products && products.length > 0 ? (
+          {products && products?.length > 0 ? (
             products.map((item, index) => (
               <Col xs={12} sm={6} md={6} lg={4} xl={3} key={index}>
                 <ProductCard product={item} />
@@ -29,29 +30,31 @@ const ProductsPage = () => {
             <p>No products available</p>
           )}
         </Row>
-        <div className="pagination">
-          <button
-            onClick={() => handlePageChange(metadata.currentPage - 1)}
-            disabled={metadata.currentPage === 1}
-          >
-            Previous
-          </button>
-          {[...Array(metadata.numberOfPages)].map((_, index) => (
+        {products.length > 0 && results > 10 && (
+          <div className="pagination" dir={locale === "ar" ? "rtl" : "ltr"}>
             <button
-              key={index}
-              onClick={() => handlePageChange(index + 1)}
-              className={metadata.currentPage === index + 1 ? "active" : ""}
+              onClick={() => handlePageChange(metadata.currentPage - 1)}
+              disabled={metadata.currentPage === 1}
             >
-              {index + 1}
+              &#10094;
             </button>
-          ))}
-          <button
-            onClick={() => handlePageChange(metadata.currentPage + 1)}
-            disabled={metadata.currentPage === metadata.numberOfPages}
-          >
-            Next
-          </button>
-        </div>
+            {[...Array(metadata.numberOfPages)].map((_, index) => (
+              <button
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                className={metadata.currentPage === index + 1 ? "active" : ""}
+              >
+                {index + 1}
+              </button>
+            ))}
+            <button
+              onClick={() => handlePageChange(metadata.currentPage + 1)}
+              disabled={metadata.currentPage === metadata.numberOfPages}
+            >
+              &#10095;
+            </button>
+          </div>
+        )}
       </Container>
     </div>
   );
