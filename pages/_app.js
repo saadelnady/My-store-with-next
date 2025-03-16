103c8bc7f3d03a484156942f77a253200d22e47d
saad elnady <saad.elnady.work@gmail.com>
2025-03-16 19:05:09 +0200

    refactor footer

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ const languages = {
 };
 
 import Header from "@/components/shared/header/Index";
-import Footer from "@/components/shared/footer";
+import Footer from "@/components/shared/footer/index.js";
 
 function App({ Component, pageProps }) {
   const router = useRouter();
